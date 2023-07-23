c5b363648f997313ed0d45df9f9a53a4d5870cb4
Kelven Fontes <kelvenbentofontes@gmail.com>
2023-07-23 20:17:16 -0300

    feat: add user reservations page

---
+++ src/app/my-trips/components/UserReservationItem.tsx
@@ -0,0 +1,64 @@
+import Image from "next/image";
+import { Prisma, TripReservation } from "@prisma/client";
+import ReactCountryFlag from "react-country-flag";
+import { format } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+import Button from "@/components/Button";
+
+interface UserReservationItemProps {
+  reservation: Prisma.TripReservationGetPayload<{
+    include: { trip: true }
+  }>
+}
+
+const UserReservationItem = ({ reservation }: UserReservationItemProps) => {
+
+  const { trip } = reservation;
+
+  return (
+    <div>
+      {/**CARD */}
+      <div className="flex flex-col p-5 mt-5 border-grayLighther border-solid border shadow-lg rounded-lg">
+        <div className="flex items-center gap-3 pb-5 border-b border-grayLighther border-solid">
+          <div className="relative h-[106px] w-[124px]">
+            <Image src={trip.coverImage} alt={trip.name} style={{ objectFit: 'cover' }} fill className="rounded-lg" />
+          </div>
+
+          <div className="flex flex-col">
+            <h2 className="text-lg text-primaryDarker font-semibold">{trip.name}</h2>
+            <div className='flex items-center gap-1'>
+              <ReactCountryFlag countryCode={trip.countryCode} svg />
+              <p className='text-xs text-grayPrimary underline'>{trip.location}</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col mt-5 text-primaryDarker">
+          <h3 className="text-sm font-medium">Data</h3>
+          <div className="flex items-center gap-1">
+            <p className="text-sm">{format(new Date(reservation.startDate), "dd 'de' MMMM", { locale: ptBR })}</p>
+            {" - "}
+            <p className="text-sm">{format(new Date(reservation.endDate), "dd 'de' MMMM", { locale: ptBR })}</p>
+          </div>
+
+          <h3 className="text-sm font-medium mt-5">Hóspedes</h3>
+          <p className="text-sm pb-5">{reservation.guests} hóspedes</p>
+
+          <h3 className="font-medium text-sm text-primaryDarker mt-1 pt-5 border-t border-solid border-grayLighther">Inormações sobre o preço</h3>
+          <div className="flex justify-between mt-1">
+            <p className="text-primaryDarker text-sm">Total:</p>
+            <p className="text-sm">R$ {Number(reservation.totalPaid)}</p>
+          </div>
+
+          <Button className="mt-5" variant="danger">Cancelar</Button>
+
+        </div>
+
+      </div>
+      {/**CARD */}
+
+    </div>
+  );
+};
+
+export default UserReservationItem;
