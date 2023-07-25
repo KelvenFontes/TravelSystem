ce496ba323fe075ceff7b164a3e6392e2c97e2fd
Kelven Fontes <kelvenbentofontes@gmail.com>
2023-07-24 22:42:57 -0300

    feat: add quick search

--- src/app/components/QuickSearch.tsx
+++ src/app/components/QuickSearch.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 const QuickSearch = () => {
   return (
@@ -11,23 +12,31 @@ const QuickSearch = () => {
 
       <div className="flex w-full justify-between mt-5">
         <div className="flex flex-col items-center gap-1">
-          <Image width={36} height={36} src='/hotel-icon.png' alt='Hotel' />
+          <Link href={`/trips/search?text=hotel`}>
+            <Image width={36} height={36} src='/hotel-icon.png' alt='Hotel' />
+          </Link>
           <p className="text-sm text-grayPrimary">Hotel</p>
         </div>
 
         <div className="flex flex-col items-center gap-1">
-          <Image width={36} height={36} src='/farm-icon.png' alt='farm' />
-          <p className="text-sm text-grayPrimary">Hotel</p>
+          <Link href={`/trips/search?text=fazenda`}>
+            <Image width={36} height={36} src='/farm-icon.png' alt='fazenda' />
+          </Link>
+          <p className="text-sm text-grayPrimary">Fazenda</p>
         </div>
 
         <div className="flex flex-col items-center gap-1">
-          <Image width={36} height={36} src='/cottage-icon.png' alt='cottage' />
-          <p className="text-sm text-grayPrimary">Hotel</p>
+          <Link href={`/trips/search?text=chalé`}>
+            <Image width={36} height={36} src='/cottage-icon.png' alt='Chalé' />
+          </Link>
+          <p className="text-sm text-grayPrimary">Chalé</p>
         </div>
 
         <div className="flex flex-col items-center gap-1">
-          <Image width={36} height={36} src='/inn-icon.png' alt='inn' />
-          <p className="text-sm text-grayPrimary">Hotel</p>
+          <Link href={`/trips/search?text=pousada`}>
+            <Image width={36} height={36} src='/inn-icon.png' alt='Pousada' />
+          </Link>
+          <p className="text-sm text-grayPrimary">Pousada</p>
         </div>
       </div>
 
