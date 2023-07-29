7a6ca537871cd73e74c83fb23574eebae2f82fd1
Kelven Fontes <kelvenbentofontes@gmail.com>
2023-07-28 22:11:58 -0300

    style: adapt search page to desktop

--- src/app/trips/search/page.tsx
+++ src/app/trips/search/page.tsx
@@ -25,11 +25,11 @@ const Trips = () => {
   }, []);
 
   return (
-    <div className="container mx-auto flex flex-col items-center p-5">
-      <h1 className="text-primaryDarker font-semibold text-xl">Viagens encontradas</h1>
-      <h2 className="text-grayPrimary font-medium mb-5">{trips.length > 0 ? 'Listamos as melhores viagens para você!' : 'Não encontramos nada de acordo com seus filtros :('}</h2>
+    <div className="container mx-auto flex flex-col items-center lg:items-start p-5">
+      <h1 className="text-primaryDarker font-semibold text-xl lg:w-full lg:text-left lg:text-[2.5rem]">Viagens encontradas</h1>
+      <h2 className="text-grayPrimary font-medium mb-5 lg:mt-6 lg:w-full lg:text-left">{trips.length > 0 ? 'Listamos as melhores viagens para você!' : 'Não encontramos nada de acordo com seus filtros :('}</h2>
 
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 lg:grid-cols-4 lg:gap-10 lg:mt-6 lg:pb-16">
         {trips?.map((trip) => (
           <TripItem key={trip.id} trip={trip} />
         ))}
