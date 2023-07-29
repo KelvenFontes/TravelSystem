5cfad5929f4c0456725f7967b5a0c7bfcf761e31
Kelven Fontes <kelvenbentofontes@gmail.com>
2023-07-28 22:51:34 -0300

    style: add grid page search

--- src/app/trips/search/page.tsx
+++ src/app/trips/search/page.tsx
@@ -29,7 +29,7 @@ const Trips = () => {
       <h1 className="text-primaryDarker font-semibold text-xl lg:w-full lg:text-left lg:text-[2.5rem]">Viagens encontradas</h1>
       <h2 className="text-grayPrimary font-medium mb-5 lg:mt-6 lg:w-full lg:text-left">{trips.length > 0 ? 'Listamos as melhores viagens para você!' : 'Não encontramos nada de acordo com seus filtros :('}</h2>
 
-      <div className="flex flex-col gap-4 lg:grid-cols-4 lg:gap-10 lg:mt-6 lg:pb-16">
+      <div className="flex flex-col gap-4 lg:grid lg:grid-cols-4 lg:gap-10 lg:mt-6 lg:pb-16">
         {trips?.map((trip) => (
           <TripItem key={trip.id} trip={trip} />
         ))}
